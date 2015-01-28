b49a64be131a89fb348c0f29053fc4f554053bf6
Hammad Hassan <hammadbinhassan@gmail.com>
2015-01-28 08:08:39 +0500

    Day 5 complete, framework interfaces are in place

---
+++ src/com/jamescho/framework/Audio.java
@@ -0,0 +1,6 @@
+package com.jamescho.framework;
+
+public interface Audio {
+	public Music createMusic(String file);
+	public Sound createSound(String file);
+}
---
+++ src/com/jamescho/framework/Sound.java
@@ -0,0 +1,6 @@
+package com.jamescho.framework;
+
+public interface Sound {
+	public void play(float volume);
+	public void dispose();
+}
